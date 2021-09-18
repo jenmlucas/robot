2e9c3c3a3df4672bb0a26f6fa7c2893b009e7931
jenmlucas <jenmlucas1@gmail.com>
2021-09-17 22:04:10 -0700

    added the function to save our robots name

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,10 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function named "fight"
+var playerName = window.prompt("What is your robot's name?");
+
+//Note the lack of quotation marks around playerNamer
+window.alert(playerName);
+
+function fight () {
+    window.alert("The fight has begun!");
+}
+fight();
